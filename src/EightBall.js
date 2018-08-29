dab55072be88882e32993d4727ca23e3502a0cf1
Clinton Owen <clint@clintonowen.com>
2018-08-29 10:43:08 -0500

    initial commit

---
+++ src/EightBall.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class EightBall extends Component {
+  render() {
+    return (
+      <div className="EightBall">
+      </div>
+    );
+  }
+}
+
+export default EightBall;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import EightBall from './EightBall';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<EightBall />, document.getElementById('root'));
+registerServiceWorker();
